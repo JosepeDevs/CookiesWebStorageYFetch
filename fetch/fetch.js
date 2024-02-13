a0a053124f46f1bfde5bf13463defdbedb95d078
josepedevs <josepedevs@gmail.com>
2024-02-13 21:12:08 +0100

    Terminamos el cienpiés de fetch-then

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -3,8 +3,7 @@ const url= "https://jsonplaceholder.typicode.com/albums" //devuelve una tabla de
 const url2= "https://dummyjson.com/users" //devuelve una tabla de titulos asociados a un usuario, cada entrada es un row
 const url3= "https://jsonplaceholder.typicode.com/photos" //devuelve una tabla de titulos asociados a un usuario, cada entrada es un row
 const tituloAbuscar= "quos omnis officia"
-const idABuscar= "quos omnis officia"
-const age= "quos omnis officia"
+const age= "29"
 const idComoAge= age
 //resultado una url
 fetch(url)
@@ -26,52 +25,61 @@ fetch(url)
     } else {
         console.log('Objeto encontrado con titulo='+tituloAbuscar+" : ", objetoEncontrado);
     }
-    const idEncontrado  = objetoEncontrado.id;
-    return fetch(url2);
+    const idEncontrado  = objetoEncontrado.userId;
+    return idEncontrado
     })
-    .then(response => {
-        return response.json();//convertir de JSON a  objetos de JS
-    })
-    .then(data2 => {//data es lo returned del then previo
-        const arrayUsuarios = data2; //lo metemos en un array
-        let userEncontrado = false
-        for(let i=0;i<arrayUsuarios.length;i++){
-            if( arrayObjetos[i].id == idABuscar){
-                userEncontrado = arrayUsuarios[i];
-                break
-            }
-        }
-
-        if (userEncontrado == false) {
-            console.log("no se encontró"+idABuscar+"en los datos proporcionados");
-        } else {
-            console.log('Objeto encontrado con id='+idABuscar+" : ", userEncontrado);
-        }
-        const ageEncontrada  = userEncontrado.age;
-        return fetch(url3);
-    })
-    .then(response => {
-        return response.json();//convertir de JSON a  objetos de JS
-    })
-    .then(data3 => {//data es lo returned del then previo
-        const arrayImagenes = data3; //lo metemos en un array
-        let imagenEncontrada = false
-        for(let i=0;i<arrayImagenes.length;i++){
-            if( arrayImagenes[i].id == ageEncontrada){
-                imagenEncontrada = arrayImagenes[i];
-                break
-            }
-        }
-
-        if (imagenEncontrada == false) {
-            console.log("no se encontró ID="+ageEncontrada+"en los datos proporcionados");
-        } else {
-            console.log('Objeto encontrado con id='+ageEncontrada+" : ", imagenEncontrada);
-        }
-        const urlImagen  = imagenEncontrada.url;
-        const imagenDiv=document.getElementById("Imagen")
-        imagenDiv.innerHTML="<i>"+urlImagen+"</i>"
-    })
-    .catch(error => {
-        console.error("hubo algún problema:", error);
-    });
+    .then(idEncontrado => {
+        const idABuscar=idEncontrado
+        fetch(url2)
+            .then(response => {
+                return response.json();//convertir de JSON a  objetos de JS
+            })
+            .then(data2=>{
+                const arrayUsuarios = data2.users; 
+                let usuarioEncontrado = false;
+                for(let i=0;i<arrayUsuarios.length;i++){
+                    if( arrayUsuarios[i].id == idABuscar){
+                        usuarioEncontrado = arrayUsuarios[i];
+                        break
+                    }
+                }
+                if (usuarioEncontrado == false) {
+                    console.log("no se encontró usuario con id "+idABuscar+" en los datos proporcionados...");
+                } else { 
+                    console.log('Usuario encontrado con id= '+idABuscar+" y "+" con edad "+usuarioEncontrado.age);
+                }
+                return usuarioEncontrado;
+            })
+            .then(data3 => {
+                const usuario=data3
+                const idABuscar2 = usuario.age
+                fetch(url3)
+                    .then(response => {
+                        return response.json();//convertir de JSON a  objetos de JS
+                    })
+                    .then(data3=>{
+                        const arrayAlbums = data3
+                        let albumEncontrado = false
+                        for(let i=0;i<arrayAlbums.length;i++){
+                            if( arrayAlbums[i].id == idABuscar2){
+                                albumEncontrado = arrayAlbums[i];
+                                break
+                            }
+                        }
+                        if (albumEncontrado == false) {
+                            console.log("no se encontró album con id "+idABuscar2+" en los datos proporcionados...");
+                        } else { 
+                            console.log('Album encontrado con id= '+idABuscar2+" y "+" con url "+albumEncontrado.url);
+                        }
+                        const enlace=albumEncontrado.url
+                        return enlace;
+                    })
+                    .then(response=>{
+                        const imagenDiv=document.getElementById("imagen")
+                        imagenDiv.innerHTML="<h2>Imagen</h2><img src='"+response +"'></img>"
+                    })
+            })
+    })        
+.catch(error => {
+    console.error("hubo algún problema:", error);
+})
